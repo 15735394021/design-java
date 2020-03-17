5fecd5714da87a24ac8540c2f0a963ae679c11b0
15735394021 <liquan1998@qq.com>
2020-03-17 21:02:22 +0800

    2020-3-17

---
+++ src/main/java/com/taru/design1/controller/Test.java
@@ -0,0 +1,13 @@
+package com.taru.design1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    @RequestMapping("/hehe")
+    public Object test(){
+
+        return "哈哈";
+    }
+}
